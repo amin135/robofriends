099aa7ecfa6c8588dd42675e5acabec55986bb41
Aminul Islam <amin13522@gmail.com>
2018-03-10 14:13:52 -0500

    First commit
    Commit

---
+++ src/robots.js
@@ -0,0 +1,80 @@
+export const robots = [
+  {
+  	id: 1,
+  	name: 'Leanne Graham',
+  	username: 'Bret',
+  	email: 'Sincere@april.biz'
+  },
+  {
+  	id: 2,
+  	name: 'Ervin Howell',
+  	username: 'Antonette',
+  	email: 'Shanna@melissa.tv'
+  },
+  {
+  	id: 3,
+  	name: 'Clementine Bauch',
+  	username: 'Samantha',
+  	email: 'Nathan@yesenia.net'
+  },
+  {
+  	id: 4,
+  	name: 'Patricia Lebsack',
+  	username: 'Karianne',
+  	email: 'Julianne.OConner@kory.org'
+  },
+  {
+  	id: 5,
+  	name: 'Lebron James',
+  	username: 'LBJ',
+  	email: 'Lebrons@april.biz'
+  },  {
+  	id: 6,
+  	name: 'Bret Hart',
+  	username: 'BretHitman',
+  	email: 'Brethart@april.biz'
+  },
+  {
+  	id: 7,
+  	name: 'Julius Irving',
+  	username: 'DocInHouse',
+  	email: 'drj@house.net'
+  },
+  {
+  	id: 8,
+  	name: 'Shaquille O Neal',
+  	username: 'ShaqAttack',
+  	email: 'shaq@yesenia.net'
+  },
+  {
+  	id: 9,
+  	name: 'Kobe Bryant',
+  	username: 'Kbyt24',
+  	email: 'KobeBryant@karina.biz'
+  },
+  {
+  	id: 10,
+  	name: 'Sinead O Conor',
+  	username: 'Moriah.Stanton',
+  	email: 'Rey.Padberg@karina.biz'
+  },
+  {
+    id: 11,
+    name: 'Sinead O Conor',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  },
+  {
+    id: 12,
+    name: 'Sinead O Conor',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+
+
+
+
+
+
+
+]
